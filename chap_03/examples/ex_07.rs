61fe27322110c2f2e5d4fe91e104bc67f3c2847c
NaoEhSavio <savioscmendes@gamil.com>
2022-11-28 15:03:57 -0300

    complete chap_03 ex_07

---
+++ chap_03/examples/ex_07.rs
@@ -0,0 +1,12 @@
+fn main(){
+	//adição
+	let soma = 5 + 10;
+	// subtração
+	let diferenca = 95.5 - 4.3;
+	//multiplicação
+	let produto = 4 * 30;
+	//divisão
+	let quociente = 56.7 / 32.2;
+	//resto
+	let resto = 43 % 5;
+}
