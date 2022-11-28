8eda8a131d22588fcf683fff345d9f3a00a67a76
NaoEhSavio <savioscmendes@gamil.com>
2022-11-28 15:05:43 -0300

    complete chap_03 ex_27

---
+++ chap_03/examples/ex_27.rs
@@ -0,0 +1,6 @@
+#![allow(unused)]
+fn main(){
+	// Então, estamos fazendo algo complicado aqui, tempo suficiente para que pericisamos
+	// várias linhas de comentários para fazer isso! Ufa! Espero que este comentário
+	// explique o que está acontecendo
+}
