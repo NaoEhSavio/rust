353f9a8840865064da6050c221c464450e48e942
NaoEhSavio <savioscmendes@gamil.com>
2022-11-28 15:03:38 -0300

    complete chap_03 ex_04

---
+++ chap_03/examples/ex_04.rs
@@ -0,0 +1,9 @@
+#![allow(unused)]
+fn main (){
+	// let espacos = "   ";
+	// let espacos = espacos.len();
+
+	let mut espacos = "   ";
+	espacos = espacos.len();
+	println!("tem {} espaços", espacos)
+}
